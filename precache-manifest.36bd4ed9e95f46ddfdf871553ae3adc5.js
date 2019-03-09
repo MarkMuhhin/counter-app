b13b25ac25d45639a6814cc39d4184956e8dbf3f
Mark Muhhin <tennnex@ut.ee>
2019-03-09 13:12:23 +0200

    Updates

---
+++ precache-manifest.36bd4ed9e95f46ddfdf871553ae3adc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c8baccf378a59c20c0",
+    "url": "/counter-app/static/js/runtime~main.29e7be9c.js"
+  },
+  {
+    "revision": "45c7f70d007ca1ebaeb5",
+    "url": "/counter-app/static/js/main.e11ffd03.chunk.js"
+  },
+  {
+    "revision": "002e0ee82dde3d67e8a7",
+    "url": "/counter-app/static/js/2.c770188a.chunk.js"
+  },
+  {
+    "revision": "45c7f70d007ca1ebaeb5",
+    "url": "/counter-app/static/css/main.2c20dfdc.chunk.css"
+  },
+  {
+    "revision": "002e0ee82dde3d67e8a7",
+    "url": "/counter-app/static/css/2.9cfc184d.chunk.css"
+  },
+  {
+    "revision": "f828285c22b9f514ae8204c2aca85ab3",
+    "url": "/counter-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/counter-app/precache-manifest.893b325aadfbc67e515fca42f1225ad0.js"
+  "/counter-app/precache-manifest.36bd4ed9e95f46ddfdf871553ae3adc5.js"
 );
 
 workbox.clientsClaim();
